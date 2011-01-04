3bc14a6b154e0cffb20ed0d02bcb6c7bb8c525d5
Will Taff @pluto <wrtaff@gmail.com>
2011-01-04 09:35:09 -0800

    fm pluto, clean up documentation

--- src/mimicClient/ClientProgram.java
+++ src/mimicClient/ClientProgram.java
@@ -1,19 +1,35 @@
-package mimicClient;
+/****************************************************************/
+/*                                                              */
+/*      Program:  Malware Mimic Client					        */
+/*                                                              */
+/*      Handles client side communications.  Controls session   */
+/*      with remote server.  Executes commands from server on   */
+/* 		local machine.                                          */
+/*                                                         		*/
+/*      FILE:       ClientProgram.java			                */
+/*                                                              */
+/*      USAGE:      ./MM-Client hostname serverName serverPort  */
+/*                                                              */
+/*          hostname      name of host                          */
+/*          serverName    IP addr of server, in dotted quad     */
+/*          serverPort 	  Integer port number of remote server  */
+/*                                                              */
+/****************************************************************/
 
+package mimicClient;
 
 
 /**
  * The MM-Client software for remote host.
- * So far, a single class that handles both sides of 
- * communication with the remote server (up and down) as 
- * well as local execution of remotely (server) commanded
- * methods.  
+ * Handles both sides of  communication with the remote server
+ * (up and down) as well as local execution of remotely (server)
+ *  commanded methods.    
  * 
  * @author W. Taff and P. Salevski
  *
  */
 public class ClientProgram {
-
+	//TODO change input params to use local host addr, instead of input host?  
 	/**
 	 * Top level main() for program.  
 	 * @param args
@@ -22,18 +38,16 @@ public class ClientProgram {
 	public static void main(String[] args) {
 		
 		
-		//TODO - how can I eject from this loop?  
-		//
 		while (true) {
 			
 			try {
 
 				new clientController(args[0], args[1], Integer
 						.parseInt(args[2])).run();
+				
 
 			} catch (NumberFormatException e) {
 
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 
 			} 
@@ -45,10 +59,8 @@ public class ClientProgram {
 			}
 			
 			
-			
 			catch (Exception e) {
 
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 
 			}
@@ -66,9 +78,7 @@ public class ClientProgram {
 				}
 				
 			}
-			
-			
-			
+	
 			
 		}//end while
 		
